262513202cef0ec5f964c884b9382640ac4eac5b
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-06-11 16:46:36 +0200

    Updated home hero section

--- my-react-podcast/src/pages/Home.jsx
+++ my-react-podcast/src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import Sidebar from "../components/Sidebar";
 import React, { useEffect, useState } from "react";
 
 export default function Home() {
   return (
     <div className="home">
-      <div className="text-align-center home-hero">Home</div>
+      <div className="text-align-center home-hero">
+        Home
+        <h1>Listen to New PODS </h1>
+      </div>
     </div>
   );
 }
