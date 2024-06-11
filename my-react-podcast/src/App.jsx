d12ccb35d9cedcc986dbdd3e1dcb767f59d0501c
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-06-11 12:46:18 +0200

    Installed react router dom

--- my-react-podcast/src/App.jsx
+++ my-react-podcast/src/App.jsx
@@ -1,11 +1,15 @@
 // import React from "react";
 import "./styles/input.css";
 import "./styles/bootstrap.css";
-
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ my-react-podcast/src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Podcast App</h1>
+    </header>
+  );
+}
