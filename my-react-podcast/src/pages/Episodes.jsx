69fb8ef77be6693513bb640ee1608f5be15d42d4
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-06-15 13:31:33 +0200

    Added episodes page

--- my-react-podcast/src/pages/Episodes.jsx
+++ my-react-podcast/src/pages/Episodes.jsx
@@ -1,3 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
+
 export default function Episodes() {
+  const [episodes, setEpisodes] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchepisode, setSearchepisode] = useState("");
+
+  useEffect(() => {
+    fetch(`https://podcast-api.netlify.app/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setEpisodes(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const episodesList = episodes.map((episode) => (
+    <NavLink key={episode.id} to={`/episode/${episode.id}`}>
+      <li className="episode-card" key={episode.id}>
+        <img src={episode.image} alt={episode.title} />
+        <a
+          className="episode-title"
+          href={episode.url}
+          target="_blank"
+          rel="noreferrer"
+        >
+          {episode.title}
+        </a>
+      </li>
+    </NavLink>
+  ));
+
   return <div>Episodes</div>;
 }
