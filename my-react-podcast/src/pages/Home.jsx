3d927e4519b4673b60b6108609e09a691685eddc
Mandla7784 <156649157+Mandla7784@users.noreply.github.com>
2024-06-19 20:09:06 +0200

    Added date format for a show card

--- my-react-podcast/src/pages/Home.jsx
+++ my-react-podcast/src/pages/Home.jsx
@@ -1,22 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import "./Home.css";
 
 export default function Home() {
-  const [episodes, setEpisodes] = useState([]);
+  const [shows, setShows] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-  const [searchepisode, setSearchepisode] = useState("");
+
+  const showGenres = {
+    1: "Personal Growth",
+    2: "Investigative Journalism",
+    3: "History",
+    4: "Comedy",
+    5: "Entertainment",
+    6: "Business",
+    7: "Fiction",
+    8: "News",
+    9: "Kids and Family",
+  };
+
+  ///formating date for last updates in show card
+  const formatDate = (dateString) => {
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    return new Date(dateString).toLocaleDateString(undefined, options);
+  };
 
   useEffect(() => {
     fetch(`https://podcast-api.netlify.app/`)
       .then((res) => res.json())
       .then((data) => {
         // Simple sorting by title in alphabetical order
-        const sortedEpisodes = data.sort((a, b) => {
+        const sortedShows = data.sort((a, b) => {
           if (a.title < b.title) return -1;
           if (a.title > b.title) return 1;
           return 0;
         });
-        setEpisodes(sortedEpisodes);
+        setShows(sortedShows);
         setIsLoading(false);
       })
       .catch((error) => {
@@ -25,29 +43,26 @@ export default function Home() {
       });
   }, []);
 
-  const episodesList = episodes.map((episode) => (
-    <NavLink key={episode.id} to={`/episode/${episode.id}`}>
-      <li className="episode-card" key={episode.id}>
-        <img src={episode.image} alt={episode.title} />
-        <a
-          className="episode-title"
-          href={episode.url}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {episode.title}
-        </a>
-        <p>{episode.description.substring(0, 250)}</p>
-        <br />
-        <br />
-      </li>
-    </NavLink>
+  const showsList = shows.map((show) => (
+    <li className="show-card" key={show.id}>
+      <NavLink to={`/show/${show.id}`}>
+        <img src={show.image} alt={show.title} />
+        <h3 className="show-title">{show.title}</h3>
+      </NavLink>
+      <p>{show.description.substring(0, 100)}</p>
+      <p>
+        <strong>Genres:</strong>{" "}
+        {show.genres
+          .map((genre) => showGenres[genre] || "Unknown Genre")
+          .join(", ")}
+      </p>
+      <p>Last Updated : {formatDate(show.updated)}</p>
+    </li>
   ));
 
   return (
     <div className="home">
-      <div className="text-align-center home-hero">
-        Home
+      <div className="home-hero text-align-center">
         <h1>Listen to New PODS</h1>
         <p>
           <i className="fa-solid fa-microphone"></i> Listen to the latest
@@ -59,7 +74,7 @@ export default function Home() {
         {isLoading ? (
           <p className="loading">Loading...</p>
         ) : (
-          <ul className="episode-list">{episodesList}</ul>
+          <ul className="show-list">{showsList}</ul>
         )}
       </div>
     </div>
